a6d31ee10e2df4092eca26b367c167d8385fd70d
Matt Mason <masonmj10@gmail.com>
2020-08-17 04:33:32 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -12,8 +12,8 @@ class Waiter
     @@all
   end 
   
-    def new_meal( customer, total, tip=0)
+    def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
-  end 
+    end
   
 end
