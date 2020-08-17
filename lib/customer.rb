d2407b9dab757ebf66a42072d2279e3c504a8408
Matt Mason <masonmj10@gmail.com>
2020-08-17 04:34:12 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,16 +1,18 @@
-class Customer  
-  attr_accessor :name, :age 
-  
+class Customer
+  attr_accessor :name, :age
+ 
   @@all = []
-  def initialize (name, age)
+ 
+  def initialize(name, age)
     @name = name
     @age = age
-    @@all << self 
-  end 
-  
-  def self.all 
+    @@all << self
+  end
+ 
+  def self.all
     @@all
-  end 
+  end
+  
     def new_meal( waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
      end 
